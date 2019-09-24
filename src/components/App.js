044ab5b10ad719822e3f31d4a5b36414248403fe
genzuby <genzuby@gmail.com>
2019-09-24 19:10:11 +0900

    yarn set

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 //for App component styles and Global styles
 import styled, { createGlobalStyle } from "styled-components";
+import "../style/main.scss";
 // Component of the location input box and the list of the input
 import SearchLocation from "./SearchLocation";
 import VenueList from "./VenueList";
-// use context info to manage ranking
-// import { VenueRankingContextStore } from "../context/VenueRankingContext";
 
 const App = () => {
   // initialize state for wether the location list close or open
@@ -23,46 +22,44 @@ const App = () => {
 
   return (
     <BODY onClick={onClose}>
-      {/* <VenueRankingContextStore> */}
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
       <TITLE>Vote for Lunchplace!</TITLE>
       <SearchLocation close={closeState} openList={setList}></SearchLocation>
       <VenueList></VenueList>
-      {/* </VenueRankingContextStore> */}
     </BODY>
   );
 };
 
 //Set Global styles
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
+// const GlobalStyle = createGlobalStyle`
+//   * {
+//     box-sizing: border-box;
+//   }
 
-  body,html{
-    @import url('https://fonts.googleapis.com/css?family=Rubik&display=swap');
-    padding: 0;
-    margin: 0;
-    font-family: 'Rubik', sans-serif;
-    min-width : 400px;
-    background-image : url(/images/mainbg.jpg);
-    background-color : rgba(0,0,0,0.65);
-    background-size : cover;
-    background-blend-mode : multiply;
-    width : 100%;
-    height : 100%;
-  }
+//   body,html{
+//     @import url('https://fonts.googleapis.com/css?family=Rubik&display=swap');
+//     padding: 0;
+//     margin: 0;
+//     font-family: 'Rubik', sans-serif;
+//     min-width : 400px;
+//     background-image : url(/images/mainbg.jpg);
+//     background-color : rgba(0,0,0,0.65);
+//     background-size : cover;
+//     background-blend-mode : multiply;
+//     width : 100%;
+//     height : 100%;
+//   }
 
-  a {
-    text-decoration: none;
-  }
+//   a {
+//     text-decoration: none;
+//   }
 
-  ul,
-  li {
-    list-style: none;
-  }
+//   ul,
+//   li {
+//     list-style: none;
+//   }
 
-`;
+// `;
 
 const BODY = styled.div`
   min-height: 98vh;
